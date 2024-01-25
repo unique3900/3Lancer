b396a65938c05e0700afbe4739c6c6f911ca1ce8
parashar <parasharneupane3900@gmail.com>
2024-01-25 12:28:56 +0545

    Registration Page Created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import Hero from './components/Hero/Hero'
 import Testimonials from './components/Testimonials/Testimonials'
 import Navbar from './layout/Navbar'
-
+import { Routes, Route } from 'react-router-dom';
+import Register from './components/Authentication/Register';
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <Hero />
-      <Testimonials/>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Hero />} />
+        <Route path='/register' element={<Register />} />
+        
+      </Routes>
+      
+      {/* <Testimonials/> */}
     </div>
   )
 }
